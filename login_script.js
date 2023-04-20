f941a7ca808f53915a41d0b7dbf129e632ae4015
Mohsin Khan <mohsinkhan845@gmail.com>
2023-04-20 21:29:08 +0530

    Login UI and login button validation
    Login UI and login button validation

---
+++ login_script.js
@@ -0,0 +1,15 @@
+const btnLogin = document.getElementById("login-button");
+const containerLoginErrorMsg = document.getElementById("login-error-msg-holder");
+
+if(btnLogin != null) {
+    btnLogin.addEventListener("click", (e) => {
+        var username = document.getElementById("username-input").value;
+        var password = document.getElementById("password-input").value;
+
+        if((username == null || username === "") || (password == null || password === "")) {
+            containerLoginErrorMsg.style.display = "block";
+        } else {
+            containerLoginErrorMsg.style.display = "none";
+        }
+    });
+}
