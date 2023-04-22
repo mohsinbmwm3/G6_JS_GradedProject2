6fc51bdde904b4cb6b1fe8472ec239170a16e957
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-04-23 01:29:03 +0530

    Resume next and previous handled working on showing

--- resume/view_resume.js
+++ resume/view_resume.js
@@ -1,9 +1,14 @@
 const btnSearch = document.getElementById("search-button");
+const btnPrevious = document.getElementById("previous-button");
+const btnNext = document.getElementById("next-button");
 const containerLoginErrorMsg = document.getElementById("search-error-msg-holder");
 const lblLoginError = document.getElementById("search-error-message");
 const resumeDiv = document.getElementById("content-card-div");
 const noresumeDiv = document.getElementById("content-no-resume-div");
+const personName = document.getElementById("name");
+const appliedFor = document.getElementById("appliedFor");
 var resume = [];
+var currentPosition = 0;
 
 window.onload = () => {
     containerLoginErrorMsg.style.display = "none";
@@ -12,9 +17,51 @@ window.onload = () => {
         console.log("length " + data.resume.length + " resume " + resume.length);
         resumeDiv.style.display = "block"
         noresumeDiv.style.display = "none"
+        showButtons()
     }
 
 }
+
+
+function showButtons() {
+    console.log("current position " + currentPosition + " length " + resume.length);
+    if (currentPosition == 0) {
+        btnPrevious.style.display = "none"
+    } else {
+        btnPrevious.style.display = "inline"
+    }
+
+    if (currentPosition < resume.length) {
+        btnNext.style.display = "inline"
+    } else {
+        btnNext.style.display = "none"
+    }
+
+    updateResume();
+}
+
+function updateResume() {
+    personName.innerText = resume[currentPosition].basics.name;
+    appliedFor.innerHTML = `<small_text_heading>Applied For </small_text_heading> <small_text>${resume[currentPosition].basics.AppliedFor}</small_text>`
+}
+
+btnNext.addEventListener("click", (e) => {
+
+    if (currentPosition < resume.length) {
+        currentPosition++;
+    }
+    console.log(currentPosition)
+    showButtons();
+})
+
+
+btnPrevious.addEventListener("click", (e) => {
+    if (currentPosition > 0) {
+        currentPosition--;
+    }
+    console.log(currentPosition)
+    showButtons();
+})
 if (btnSearch != null) {
     btnSearch.addEventListener("click", (e) => {
         var searchInput = document.getElementById("search-input-field").value;
