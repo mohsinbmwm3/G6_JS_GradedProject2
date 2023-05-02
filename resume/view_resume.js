94a3cd989aef127e3eae742797eac1a85d3dcce1
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-05-02 23:51:23 +0530

    resume completed

--- resume/view_resume.js
+++ resume/view_resume.js
@@ -21,6 +21,12 @@ const project = document.getElementById("project")
 const ugEdu = document.getElementById("ugEdu")
 const SeniorEdu = document.getElementById("SeniorEdu")
 const highSchool = document.getElementById("highSchool")
+const companyName = document.getElementById("companyName")
+const InternshipPosition = document.getElementById("InternshipPosition")
+const InternshipStartDate = document.getElementById("InternshipStartDate")
+const InternshipEndDate = document.getElementById("InternshipEndDate")
+const InternshipSummary = document.getElementById("InternshipSummary")
+const Achievements = document.getElementById("Achievements")
 var resume = [];
 var currentPosition = 0;
 
@@ -87,10 +93,26 @@ function updateResume() {
     position.innerText = currentResume.work.Position;
     startDate.innerText = currentResume.work["Start Date"];
     endDate.innerText = currentResume.work["End Date"];
+    var achievements = ""
+    for (items in currentResume.achievements.Summary) {
+        if (items > 0) {
+            items += "\n"
+        }
+        achievements += currentResume.achievements.Summary[items]
+
+    }
+    Achievements.innerText = achievements;
 
     ugEdu.innerHTML = `<div><p><b>UG</b> ${currentResume.education.UG.institute}, ${currentResume.education.UG.course} ${currentResume.education.UG["Start Date"]} ${currentResume.education.UG["End Date"]}}<br></p><div>`;
     SeniorEdu.innerHTML = `<div><p><b>PU</b> ${currentResume.education["Senior Secondary"].institute}, ${currentResume.education["Senior Secondary"].cgpa}</p><div>`;
     highSchool.innerHTML = `<div><p><b>High School</b> ${currentResume.education["High School"].institute}, ${currentResume.education["High School"].cgpa}</p><div>`;
+
+
+    companyName.innerHTML = `<div><p><b>Company Name: </b> ${currentResume.Internship["Company Name"]}</p><div>`;
+    InternshipPosition.innerHTML = `<div><p><b>Position: </b> ${currentResume.Internship["Position"]}</p><div>`;
+    InternshipStartDate.innerHTML = `<div><p><b>Start Date: </b> ${currentResume.Internship["Start Date"]}</p><div>`;
+    InternshipEndDate.innerHTML = `<div><p><b>End Date: </b> ${currentResume.Internship["End Date"]}</p><div>`;
+    InternshipSummary.innerHTML = `<div><p><b>Summary: </b> ${currentResume.Internship["Summary"]}</p><div>`;
 
 }
 
